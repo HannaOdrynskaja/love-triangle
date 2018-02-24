4b9fd6f7262387e0640f06cb8fe1a0d29759531d
Hanna Odrynskaja <36698550+HannaOdrynskaja@users.noreply.github.com>
2018-02-24 14:17:16 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  
+var count=0;
+l=preferences.length;
+for (i=1;i<=l;i++){
+var a1 = i;
+var a2 = preferences[a1-1];
+var a3 = preferences[a2-1];
+var a4 = preferences[a3-1];
+
+if (a1 == a4){
+	count++;
+	console.log(a1+" loves " + a2 + ", loves "+ a3+", loves "+a4+".  count= "+ count);	
+	}else{
+	console.log(a1+" - " + a2 + " - " + a3 + " - "+a4+ " - no");
+} 
+}
+return("rezult = "+count/3);
 };
